622a395fe4b6e6902f35a12da232a88059323dcb
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-12-04 15:06:22 +0530

    `Logged In User data `

--- src/employee/loggedUser/LoggedInUser.java
+++ src/employee/loggedUser/LoggedInUser.java
@@ -4,14 +4,68 @@
  */
 package employee.loggedUser;
 
+import java.sql.ResultSet;
+
 /**
  *
  * @author pande
  */
 public class LoggedInUser {
-    String name = null;
+    String name ;
+    String email , password , gender , phoneNo , module;
+    ResultSet rs;
 
+//    public void setRs(ResultSet rs) {
+//        this.rs = rs;
+//        try{
+//            this.name = rs.getString("name");
+//            System.out.println(name);
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getModule() {
+        return module;
+    }
     public String getName() {
+//        System.out.println(name+"getName fun called");
         return name;
     }
 
